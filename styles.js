46be736e0d4069dd508b38e17decd64051964381
Vishvam <s.vishvam1025@gmail.com>
2022-08-04 00:17:58 +0530

    Added global styles

--- styles.js
+++ styles.js
@@ -1,23 +1,34 @@
 const STYLES = `
 
 :root {
-    --color-primary: #5ec576;
+    --color-primary: #0d6efd;
     --color-secondary: #ffcb03;
     --color-tertiary: #ff585f;
-    --color-primary-darker: #4bbb7d;
+    --color-primary-darker: #0b5dd7;
     --color-secondary-darker: #ffbb00;
     --color-tertiary-darker: #fd424b;
     --color-primary-opacity: #5ec5763a;
     --color-secondary-opacity: #ffcd0331;
+    --color-grey-light-3: #f2f2f2;
+    --color-grey-light-2: #d0d0d0;
+    --color-grey-light-1: #9c9c9c;
+    --color-grey: #808080;
+    --color-grey-dark-1: #6c6c6c;
+    --color-grey-dark-2: #444444;
+    --color-white: #FFFFFF;
+    --color-tertiary-opacity: #ff58602d;
     --color-tertiary-opacity: #ff58602d;
     --gradient-primary: linear-gradient(to top left, #39b385, #9be15d);
     --gradient-secondary: linear-gradient(to top left, #ffb003, #ffcb03);
-    --box-shadow: box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+    --default-box-shadow-light: rgba(120, 123, 127, 0.2) 0px 8px 16px;
+    --default-box-shadow: rgba(75, 77, 80, 0.2) 0px 8px 24px;
+    /* --box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px; */
+    --default-sanserif-font: Arial, Helvetica, sans-serif;
 }
 
 ion-icon {
     font-size: 2rem;
-    color: grey;
+    color: var(--color-grey);
     transition: all 0.2s cubic-bezier(0.165, 0.84, 0.44, 1);
     cursor: pointer;
 }
@@ -31,8 +42,7 @@ ion-icon:active {
 }
 
 ion-icon[name="open-outline"] {
-    // font-size: 1.2rem;
-    color: grey;
+    color: var(--color-grey);
 }
 
 ion-icon[name="create-outline"] {
@@ -40,7 +50,7 @@ ion-icon[name="create-outline"] {
 }
 
 ion-icon[name="create-outline"]:hover {
-    color: #0d6efd;
+    color: var(--color-primary);
 }
 
 ion-icon[name="trash-outline"] {
@@ -58,13 +68,13 @@ ion-icon[name="ellipsis-horizontal-outline"] {
 ion-icon[name="paper-plane-outline"] {
     font-size: 2.8rem;
     rotate: 45deg;
-    /* color: #0d6efd; */
+    /* color: var(--color-primary); */
 }
 
 ion-icon[name="paper-plane-outline"]:hover {
     font-size: 3.2rem;
     rotate: 45deg;
-    color: #0d6efd;
+    color: var(--color-primary);
 }
 
 ion-icon[name="arrow-up-outline"] {
@@ -85,11 +95,6 @@ ion-icon[name="arrow-down-outline"] {
 ion-icon[name="arrow-down-outline"]:hover {
     font-size: 1.8rem;
     color: var(--color-tertiary);
-}
-
-mark {
-    background-color: yellow;
-    color: black;
 }
 
 .hide {
@@ -115,6 +120,7 @@ mark {
     font-size: 1.1rem;
 }
 
+
 /*
     ********************* MODAL *********************
 */
@@ -125,10 +131,10 @@ mark {
     left: 50%;
     transform: translate(-50%, -50%);
     width: 40rem;
-    background-color: white;
+    background-color: var(--color-white);
     border-radius: 1rem;
     padding: 1rem;
-    box-shadow: rgba(75, 77, 80, 0.2) 0px 8px 24px;
+    box-shadow: var(--default-box-shadow);
     z-index: 1000000;
     transition: all 0.5s;
     height: auto;
@@ -142,7 +148,7 @@ mark {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    border-bottom: 1px solid #e8e8e8;
+    border-bottom: 1px solid var(--color-grey-light-1);
     border-radius: 1rem 1rem 0rem 0rem;
 }
 
@@ -179,31 +185,37 @@ mark {
     top: 0;
     right: 0;
     width: 52rem;
-    background-color: white;
+    background-color: var(--color-white);
     border-radius: 1rem;
     padding: 1rem;
-    box-shadow: rgba(75, 77, 80, 0.2) 0px 8px 24px;
+    box-shadow: var(--default-box-shadow);
     z-index: 1000;
     height: 72rem;
-    background: white;
+    background: var(--color-white);
     padding: 1rem;
-    animation: sidebar_animation 0.6s;
+    animation: remark_sidebar_animation 0.6s;
 }
 
-@keyframes sidebar_animation {
+@keyframes remark_sidebar_animation {
     from {
         width: 0px;
     }
     to {
         width: 52rem;
     }
-  }
 }
+
+.remark_standard_sidebar_body {
+    height: 80%;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    scrollbar-width: none;
+}
+
 
 /* 
     ********************* ANNOTATIONS *********************
 */
-
 
 .resolve_btn {
     background-color: var(--color-primary);
@@ -215,9 +227,9 @@ mark {
     border-radius: 10rem;
     cursor: pointer;
     transition: all 0.3s;
-    border-radius: 1rem; 
+    border-radius: 1rem;
     width: 10rem;
-}   
+}
 
 .resolve_btn:hover {
     background-color: var(--color-primary-darker);
@@ -227,8 +239,8 @@ mark {
     margin: 1.6rem 0rem 2rem 0rem;
     height: auto;
     padding: 1rem;
-    border: 1px solid #e8e8e8;
-    border-radius: 1rem; 
+    border: 1px solid var(--color-grey-light-1);
+    border-radius: 1rem;
     font-family: "arial";
 }
 
@@ -241,7 +253,6 @@ mark {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    /* background-color: red; */
 }
 
 .remark_comment_actions {
@@ -250,7 +261,6 @@ mark {
     justify-content: space-between;
     align-items: center;
     width: 20%;
-    /* background-color: #0d6efd; */
 }
 
 .remark_annotation_reply {
@@ -265,7 +275,7 @@ mark {
     width: 1rem;
     padding: 1rem;
     height: 4rem;
-    box-shadow: rgb(120 123 127 / 20%) 0px 8px 16px;
+    box-shadow: var(--default-box-shadow);
     border-radius: 1rem;
     position: relative;
     top: 15%;
@@ -293,7 +303,7 @@ mark {
     justify-content: center;
     align-items: start;
     height: 100%;
-    color: gray;
+    color: var(--color-grey);
 }
 
 .remark_annotation_user_username {
@@ -311,7 +321,7 @@ mark {
 .remark_annotation_user_message {
     width: 97%;
     margin: 1rem 0rem 1rem 0rem;
-    color: grey;
+    color: var(--color-grey);
     padding: 0rem 0rem 0rem 1rem;
     font-family: inherit;
     font-size: 1.3rem;
@@ -327,45 +337,38 @@ mark {
     margin: 2rem 1rem 1rem 1rem;
 }
 
+
 /* 
-    ********************* INPUT *********************
+    ********************* INPUT FIELDS *********************
 */
 
 textarea {
     padding: 2rem;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: var(--default-sanserif-font);
     -webkit-appearance: none;
     -moz-appearance: none;
     height: 6rem;
     width: 90%;
     border-radius: 1.4rem;
-    background-color: white;
+    background-color: var(--color-white);
     resize: none;
     overflow-y: scroll;
     outline: 0px !important;
     scrollbar-width: none;
     margin: 1rem;
     transition: transform 0.2s ease-in;
-    border: 1px solid #9c9c9c;
+    border: 1px solid var(--color-grey-light-1);
 }
 
 textarea:hover {
-    border: 0.5px solid #444444;
+    border: 0.5px solid var(--color-grey-dark-2);
 }
 
 textarea:focus {
-    border: 0.5px solid #0d6efd;
-}
-
-.remark_standard_sidebar_body {
-    height: 80%;
-    overflow-x: hidden;
-    overflow-y: scroll;
-    scrollbar-width: none;
+    border: 0.5px solid var(--color-primary);
 }
 
 .remark_annotation_user_input {
-    /* background-color: black; */
     position: absolute;
     bottom: 1rem;
     margin: 2rem 0rem 0rem 0rem;
@@ -379,8 +382,8 @@ textarea:focus {
 .signup_container {
     height: auto;
     width: 20rem;
-    background-color: white;
-    box-shadow: rgb(120 123 127 / 20%) 0px 8px 16px;
+    background-color: var(--color-white);
+    box-shadow: var(--default-box-shadow-light);
     border-radius: 1rem;
     padding: 2rem;
 }
@@ -395,30 +398,30 @@ textarea:focus {
 
 .remark_form_input {
     padding: 1rem 2rem 1.2rem 1rem;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: var(--default-sanserif-font);
     -webkit-appearance: none;
     -moz-appearance: none;
     height: 4rem;
     width: 100%;
     border-radius: 0.6rem;
-    background-color: white;
+    background-color: var(--color-white);
     border: none;
     outline: 0px !important;
     margin: 0.4rem 0rem 1rem 0rem;
     transition: border 0.2s ease-in;
-    border: 0.5px solid rgb(142, 142, 142);
+    border: 0.5px solid var(--color-grey);
     font-size: 1.2rem;
-    color: grey;
+    color: var(--color-grey);
 }
 
 .remark_form_input:focus {
-    border: 0.5px solid #0d6efd;
+    border: 0.5px solid var(--color-primary);
 }
 
 .remark_form_label {
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: var(--default-sanserif-font);
     font-size: 1.2rem;
-    color: grey;
+    color: var(--color-grey);
 }
 
 .remark_standard_button {
@@ -432,7 +435,7 @@ textarea:focus {
     cursor: pointer;
     transition: all 0.1s;
     margin: 3rem 0rem 2rem 0rem;
-    width: 100%;   
+    width: 100%;
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -449,7 +452,7 @@ textarea:focus {
 
 .remark_delete_button {
     background-color: var(--color-tertiary);
-    color: white;
+    color: var(--color-white);
 }
 
 .remark_resolve_button {
@@ -461,11 +464,11 @@ textarea:focus {
 
 .remark_unresolve_button {
     background-color: var(--color-tertiary);
-    color: white;
+    color: var(--color-white);
 }
 
 .error_modal {
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: var(--default-sanserif-font);
     font-size: 1rem;
     line-height: 1.4rem;
     height: auto;
@@ -473,11 +476,11 @@ textarea:focus {
     box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
     border-radius: 0.4rem;
     padding: 0.8rem 1.4rem 1rem 1.6rem;
-    color: white;
+    color: var(--color-white);
 }
 
 .highlight_element_light {
-    outline: dashed 2px grey;    
+    outline: dashed 2px var(--color-grey);
     border-radius: 0.4rem;
     padding: 0.4rem;
 }
@@ -495,9 +498,9 @@ textarea:focus {
     width: 22rem;
     height: auto;
     border-radius: 1rem;
-    background: #fff;
+    background: var(--color-white);
     box-shadow: 0 12px 35px rgba(0, 0, 0, 0.1);
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: var(--default-sanserif-font);
     padding: 1rem;
     z-index: 100000;
 }
@@ -530,7 +533,7 @@ textarea:focus {
 
 .remark_contextmenu_icon {
     font-size: 2rem !important;
-    color: grey !important;
+    color: var(--color-grey) !important;
 }
 
 .remark_contextmenu_content .remark_item:hover {
@@ -546,7 +549,7 @@ textarea:focus {
 .remark_context_menu_item_name {
     margin: 1.2rem 0rem 1rem 1.2rem;
     font-size: 1.4rem;
-    color: grey;
+    color: var(--color-grey);
 }
 
 `
