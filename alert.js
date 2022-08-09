42c0ba9a51242e15e97a05b950ce8c9e204b5438
Vishvam <s.vishvam1025@gmail.com>
2022-08-09 23:38:03 +0530

    Updated errors to alerts

---
+++ alert.js
@@ -0,0 +1,44 @@
+const alertModal = (type, message) => {
+    let markup = ""
+    switch (type) {
+        case "ERROR":
+            markup = `
+                <div class="remark_error_container remark_error_container_error">
+                    <p class="remark_error_text">${message}</p>
+                </div>
+                `
+            break;
+        case "SUCCESS":
+            markup = `
+                <div class="remark_error_container remark_error_container_success">
+                    <p class="remark_error_text">${message}</p>
+                </div>
+            `
+            break
+        case "INTIMATION":
+            markup = `
+            <div class="remark_error_container remark_error_container_intimation">
+                <p class="remark_error_text">${message}</p>
+            </div>
+            `
+            break
+        default:
+            markup = ""
+    }
+    return markup;
+}
+
+function hideAlert() {
+    const ele = document.querySelector('.remark_error_container');
+    if (ele) {
+        removeHTMLElement(ele)
+    }
+}
+
+function showAlert(type, message, time = 1) {
+    hideAlert();
+    const markup = alertModal(type, message);
+    console.log(type, message);
+    document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+    window.setTimeout(hideAlert, time * 1000);
+}
